d595d27679ab9362ca7f8cdbd4dff9cd6e8a484c
Oskar-Ciebien <G00369579@gmit.ie>
2021-02-22 23:10:58 +0000

    Added Main Menu with Buttons

---
+++ Assets/__Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Play - Starts the game, putting the player to level 1
+    public void Play()
+    {
+        SceneManager.LoadScene("Level_1");
+    } // Play - END
+    
+    // Exit - Exits the game
+    public void Exit()
+    {
+        Application.Quit();
+    } // Exit - END
+}
