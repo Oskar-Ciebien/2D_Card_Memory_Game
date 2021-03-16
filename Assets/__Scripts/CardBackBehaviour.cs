f652da4ba9c9006b115a6aae41e9c7e2be4ba8bc
Oskar-Ciebien <G00369579@gmit.ie>
2021-03-16 20:46:19 +0000

    Added backs of cards and disappearing functionality for them

---
+++ Assets/__Scripts/CardBackBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBackBehaviour : MonoBehaviour
+{
+    // OnMouseClick
+    void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+}
