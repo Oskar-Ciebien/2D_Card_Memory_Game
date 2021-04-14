3be3a720a6394f03d2d5e3843db8bfc70c3eb7c5
David Newman <48318621+DavidNewmanGMIT@users.noreply.github.com>
2021-04-14 09:22:33 +0100

    Database
    Had to redo whole database as Mysql would not work with newest version of Unity.
    Database is now done using Sqlite, database is made and is reading to app, but now having problem inserting data into high score board.

---
+++ Assets/__Scripts/HighScoreManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+public class HighScoreManager : MonoBehaviour
+{
+    private string connectionString;
+
+    private List<Highscore> highscores = new List<Highscore>();
+
+    public GameObject scorePrefab;
+
+    public Transform scoreParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        connectionString = "URI=file:" + Application.dataPath + "/HighscoreDB.db";
+        //InsertScore("mandy", 29);
+        //GetScores();
+        ShowScores();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //adding scores to database
+    private void InsertScore(string name, int newScore)
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("INSERT INTO highscore(name,score) VALUES(\"{0}\", \"{1}\")", name, newScore);
+
+                dbCmd.CommandText = sqlQuery;
+                dbCmd.ExecuteScalar();
+                dbConnection.Close();
+
+            }
+        }
+    }
+
+    //return database test
+    private void GetScores()
+    {
+        //clear list
+        highscores.Clear();
+
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM highscore";
+
+                dbCmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        highscores.Add(new Highscore(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));
+                    }
+
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+    }
+
+    private void ShowScores()
+    {
+        GetScores();
+
+        for (int i = 0; i < highscores.Count; i++)
+        {
+            GameObject tmpObjec = Instantiate(scorePrefab);
+
+            Highscore tmpScore = highscores[i];
+
+            tmpObjec.GetComponent<HighScoreScript>().SetScore(tmpScore.Name, tmpScore.Score.ToString(), "a" + (i + 1).ToString());
+
+            tmpObjec.transform.SetParent(scoreParent);
+        }
+    }
+}
---
+++ Assets/__Scripts/HighScoreScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreScript : MonoBehaviour
+{
+    public GameObject score;
+    public GameObject scoreName;
+    public GameObject rank;
+
+    public void SetScore(string name, string score, string rank)
+    {       
+        this.rank.GetComponent<Text>().text = rank;
+        this.scoreName.GetComponent<Text>().text = name;
+        this.score.GetComponent<Text>().text = score;
+    }
+}
